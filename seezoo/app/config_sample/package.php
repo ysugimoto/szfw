cbd856eb963b49e6aa7edc06cac30f1586691b36
ysugimoto <neo.yoshiaki.sugimoto@gmail.com>
2012-10-18 20:30:16 +0900

    Added package config file

---
+++ seezoo/app/config_sample/package.php
@@ -0,0 +1,31 @@
+<?php if ( ! defined('SZ_EXEC') ) exit('access_denied');
+
+/**
+ * ====================================================================
+ * 
+ * Seezoo-Framework
+ * 
+ * A simple MVC/action Framework on PHP 5.1.0 or newer
+ * 
+ * 
+ * Package settings
+ * 
+ * @package  Seezoo-Framework
+ * @category config
+ * @author   Yoshiaki Sugimoto <neo.yoshiaki.sugimoto@gmail.com>
+ * @license  MIT Licence
+ * 
+ * ====================================================================
+ */
+
+/*
+ * --------------------------------------------------
+ * package
+ * 
+ * Framework routing from subpackge system.
+ * Controller, helper, model, library, view and some class
+ * detect subpackage.
+ * --------------------------------------------------
+ */
+
+$package = array();
